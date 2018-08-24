eceed7dd7924185eaeb643d48cefc7bec6596ff8
zpaki5142 <zachisaac@users.noreply.github.com>
2018-08-23 23:36:57 -0700

    Second Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Crystal Game
+
+//Variables
+//Random Number Variable
+var randomNumber = Math.floor((Math.random() * 120) + 19);
+
+//Actual Number Variable
+var actualNumber = Math.floor((Math.random() * 12) + 1);
+
+//Wins Variable
+var win;
+
+//Losses Variable
+var lose;
+//Pick a random number between 19 and 120
+//Gems worth random number between 1 and 12
+//Wins
+//Losses
+//Your total score at the moment
