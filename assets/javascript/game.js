dacd4562f5185b4e6b49fcad855eaf820031b5ed
zpaki5142 <zachisaac@users.noreply.github.com>
2018-08-24 00:53:22 -0700

    Third Commit/Added Images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,22 @@
 //Crystal Game
 
 //Variables
+//Pick a random number between 19 and 120
 //Random Number Variable
 var randomNumber = Math.floor((Math.random() * 120) + 19);
+console.log(randomNumber);
 
+//Gems worth random number between 1 and 12
 //Actual Number Variable
 var actualNumber = Math.floor((Math.random() * 12) + 1);
+console.log(actualNumber);
 
 //Wins Variable
 var win;
 
 //Losses Variable
 var lose;
-//Pick a random number between 19 and 120
+
 //Gems worth random number between 1 and 12
 //Wins
 //Losses
